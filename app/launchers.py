9cffd923baa0b4de469673f067e3971ebcdb4866
Guillaume Müller <guillaume.muller@heig-vd.ch>
2025-04-22 09:12:50 +0200

    Improved `launchers.py`: - added header docstring - corrected `launch_prod_mode` function checking the wrong folder - improved dev mode:   - added server requests before opening Eel window   - put server requests and moved process termination functions in a new `utils` module

--- app/launchers.py
+++ app/launchers.py
@@ -1,6 +1,15 @@
-from . import backend  # Needed for Eel-exposed functions
+"""
+This module is responsible for launching Eel and Vite.
+
+Functions depending on others check for the presence of the folders the latters create to eventually call them.
+
+Most constants are defined to match Node.js and Vite defaults and options set in `vite.config.js`.
+"""
+
+from . import backend  # Eel-exposed functions
+from . import utils
 import eel, eel.types
-import psutil, subprocess
+import subprocess
 from pathlib import Path
 import sys
 from typing import Iterable, Dict, Callable, Any
@@ -8,10 +17,11 @@ from typing import Iterable, Dict, Callable, Any
 THIS_FILE_FOLDER_PATH = Path(__file__).resolve().parent
 WEB_FILES_PATH = THIS_FILE_FOLDER_PATH / 'frontend'
 WEB_INSTALLS_PATH = WEB_FILES_PATH / 'node_modules'
-WEB_PROD_PATH = WEB_FILES_PATH / 'build'
+WEB_PROD_BUILD_PATH = WEB_FILES_PATH / 'build'
 WEB_PROD_INDEX_FILE_NAME = 'index.html'
-WEB_DEV_PATH = WEB_FILES_PATH / 'src'
-WEB_DEV_FILES_EXTENSIONS = ('.vue', '.js')  # To search Eel-exposed functions
+WEB_DEV_SRC_PATH = WEB_FILES_PATH / 'src'
+WEB_DEV_FILES_EXTENSIONS = ('.vue', '.js')  # To search Eel-exposed functions in dev mode
+WEB_DEV_LAUNCH_TIMEOUT = 30  # [s], timeout for the Vite dev server to start
 
 VITE_DEV_SERVER_HOST = 'localhost'
 VITE_DEV_SERVER_PORT = 5173
@@ -23,35 +33,6 @@ def install_frontend_dependencies():
     if 0 != subprocess.call("npm install".split(), cwd=WEB_FILES_PATH):
         raise RuntimeError("`npm install` failed")
 
-def launch_dev_mode():
-    if not WEB_INSTALLS_PATH.exists():
-        install_frontend_dependencies()
-    print("Launching frontend in development mode...")
-    npm_process = subprocess.Popen(  # Asynchronous
-        "npm run dev".split(),
-        cwd=WEB_FILES_PATH
-    )
-    if npm_process.poll() is not None:
-        raise RuntimeError("`npm run dev` stopped unexpectedly")
-
-    def close_callback(page: str, sockets: eel.types.WebSocketT):
-        process = psutil.Process(npm_process.pid)
-        children = process.children(recursive=True)
-        for child in children : child.terminate()
-        psutil.wait_procs(children)
-        npm_process.terminate()
-        npm_process.wait()
-        sys.exit(0)
-
-    _start_eel(
-        init_path=WEB_DEV_PATH,
-        start_urls={
-            'host': VITE_DEV_SERVER_HOST,
-            'port': VITE_DEV_SERVER_PORT
-        },
-        close_callback=close_callback
-    )
-
 def build_frontend():
     if not WEB_INSTALLS_PATH.exists():
         install_frontend_dependencies()
@@ -60,10 +41,38 @@ def build_frontend():
         raise RuntimeError("`npm run build` failed")
 
 def launch_prod_mode():
-    if not WEB_INSTALLS_PATH.exists():
+    if not WEB_PROD_BUILD_PATH.exists():
         build_frontend()
     print("Launching frontend in production mode...")
-    _start_eel(init_path=WEB_PROD_PATH, start_urls=WEB_PROD_INDEX_FILE_NAME)
+    _start_eel(
+        init_path=WEB_PROD_BUILD_PATH,
+        start_urls=WEB_PROD_INDEX_FILE_NAME
+    )
+
+def launch_dev_mode():
+    if not WEB_INSTALLS_PATH.exists():
+        install_frontend_dependencies()
+    print("Launching frontend in development mode...")
+    npm_process = subprocess.Popen("npm run dev".split(), cwd=WEB_FILES_PATH)  # Async
+
+    print("Waiting for Vite dev server to start...")
+    if not utils.block_until_server_is_alive(
+        f"http://{VITE_DEV_SERVER_HOST}:{VITE_DEV_SERVER_PORT}",
+        timeout=WEB_DEV_LAUNCH_TIMEOUT
+    ):
+        utils.terminate_process_and_children(npm_process.pid)
+        raise RuntimeError(
+            f"Vite dev server failed to start within {WEB_DEV_LAUNCH_TIMEOUT} seconds"
+        )
+
+    _start_eel(
+        init_path=WEB_DEV_SRC_PATH,
+        start_urls={"host": VITE_DEV_SERVER_HOST, "port": VITE_DEV_SERVER_PORT},
+        close_callback=lambda page, sockets: (
+            utils.terminate_process_and_children(npm_process.pid),
+            sys.exit(0),
+        )
+    )
 
 def _start_eel(
     init_path: str | Path,
